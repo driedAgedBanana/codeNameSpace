138475436004fdc419a5d08ba918f1d596f9e29e
Wyzard12 <b.cao@sintlucasedu.nl>
2023-03-22 13:49:11 +0100

    Small changes

--- CodenameSpace/CodenameSpace/Assets/Scripts/enemyPatrol.cs
+++ CodenameSpace/CodenameSpace/Assets/Scripts/enemyPatrol.cs
@@ -4,38 +4,105 @@ using UnityEngine;
 
 public class enemyPatrol : MonoBehaviour
 {
-        [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float moveSpeed = 5f;
 
-        Rigidbody2D enemyRB;
-        BoxCollider2D enemyBox;
+    Rigidbody2D enemyRB;
+    BoxCollider2D enemyBox; 
+    public Animator Anim;
+    bool Attacking;
 
-        void Start()
-        {
+
+    void Start()
+    {
             enemyRB = GetComponent<Rigidbody2D>();
             enemyBox= GetComponent<BoxCollider2D>();
-        }
+    }
 
-        void Update()
+    void Update()
+    {
+        if (!Attacking)
         {
             if (isFacingLeft())
             {
+                //check for a wall in front of the enemy
+                Vector2 rayOrigin = new Vector2(enemyBox.bounds.min.x - 0.1f, enemyBox.bounds.center.y);
+                RaycastHit2D hitWall = Physics2D.Raycast(rayOrigin, Vector2.left, 0.2f, LayerMask.GetMask("Wall"));
+
+                //check for ground beneath the enemy
+                rayOrigin = new Vector2(enemyBox.bounds.center.x, enemyBox.bounds.min.y - 0.1f);
+                RaycastHit2D hitGround = Physics2D.Raycast(rayOrigin, Vector2.down, 0.2f, LayerMask.GetMask("Ground"));
+
+                if (hitWall.collider != null || hitGround.collider == null)
+                {
+                    //change direction if a wall is detected or there is no ground beneath the enemy
+                    transform.localScale = new Vector2(-1 * transform.localScale.x, transform.localScale.y);
+                }
+
                 //walk to the left
-                enemyRB.velocity = new Vector2(moveSpeed, 0f);
+                if(!Attacking)
+                {
+                    enemyRB.velocity = new Vector2(moveSpeed, 0f);
+                    print("WalkL");
+                }
+
             }
             else
             {
+                //check for a wall in front of the enemy
+                Vector2 rayOrigin = new Vector2(enemyBox.bounds.max.x + 0.1f, enemyBox.bounds.center.y);
+                RaycastHit2D hitWall = Physics2D.Raycast(rayOrigin, Vector2.right, 0.2f, LayerMask.GetMask("Wall"));
+
+                //check for ground beneath the enemy
+                rayOrigin = new Vector2(enemyBox.bounds.center.x, enemyBox.bounds.min.y - 0.1f);
+                RaycastHit2D hitGround = Physics2D.Raycast(rayOrigin, Vector2.down, 0.2f, LayerMask.GetMask("Ground"));
+
+                if (hitWall.collider != null || hitGround.collider == null)
+                {
+                    //change direction if a wall is detected or there is no ground beneath the enemy
+                    transform.localScale = new Vector2(-1 * transform.localScale.x, transform.localScale.y);
+                }
+
                 //move to the left
-                enemyRB.velocity = new Vector2(-moveSpeed, 0f);
+                if (!Attacking)
+                {
+                    enemyRB.velocity = new Vector2(-moveSpeed, 0f);
+                    print("Walk");
+                }
             }
         }
-
-        private bool isFacingLeft()
+        else
         {
-            return transform.localScale.x < Mathf.Epsilon;
+            enemyRB.velocity = Vector2.zero;
         }
+    }
 
-        private void OnTriggerExit2D(Collider2D collision)
+    private bool isFacingLeft()
+    {
+        return transform.localScale.x < Mathf.Epsilon;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        
+        if (collision.gameObject.GetComponent<Playermovement>())
+        {
+            Anim.SetBool("Attacking", false);
+            Anim.SetBool("isWalking", true);
+            Attacking = false;
+        }
+        else
         {
             transform.localScale = new Vector2(-(Mathf.Sign(enemyRB.velocity.x)), transform.localScale.y);
         }
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<Playermovement>())
+        {
+            Anim.SetBool("isWalking", false);
+            Anim.SetBool("Attacking", true);
+            Attacking = true;
+        }
+    }
+}
