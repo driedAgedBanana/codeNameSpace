2cd3bf96d826048ffb2a075fc088d7e2fdc67195
Wyzard12 <b.cao@sintlucasedu.nl>
2023-03-28 08:34:04 +0100

    Changing Menu

--- CodenameSpace/CodenameSpace/Assets/Scripts/MenuScript.cs
+++ CodenameSpace/CodenameSpace/Assets/Scripts/MenuScript.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour
 {
+    public string firstLevel;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +18,26 @@ public class MenuScript : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("mainScene");
+    }
+
+    public void OpenOption()
+    {
+
+    }
+
+    public void CloseOption()
+    {
+
+    }
+
+    public void CloseGame()
+    {
+        Application.Quit(0);
+        Debug.Log("I quit");
     }
 }
